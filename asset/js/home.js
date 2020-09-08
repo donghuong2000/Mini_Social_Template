954d061d998e2c7a20207363ac969c68d72672a0
Nguyen Dong Huong <donghuong2000@users.noreply.github.com>
2020-09-08 09:47:11 +0700

    add some amimation

---
+++ asset/js/home.js
@@ -0,0 +1,14 @@
+var sf = document.getElementById("searchForm")
+
+sf.onmouseenter = function(){
+    sf.style.width = "400px"
+    sf.style.transition ="0.3s"
+}
+sf.onmouseleave = function(){
+    sf.style.transition ="0.3s"
+    sf.style.width = "200px"
+
+}
+
+
+
