fe305686738ece03fb5a81b156f332896f87b72c
Nguyen Dong Huong <donghuong2000@users.noreply.github.com>
2020-09-11 23:58:27 +0700

    update

--- asset/js/ruongdo.js
+++ asset/js/ruongdo.js
@@ -5,9 +5,22 @@ function allowDrop(ev) {
   function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
   }
-  
+  var image =[ "/images/z.png","/images/yy.png"];
   function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
+    var b = document.getElementById(data).getAttribute("src");
+    if(!image.includes(b))
+    {
+      image.push(b)
+    }
+    image.sort();
+    var l="";
+    image.forEach(element => {
+      l+="url("+element+"),"
+    });
+    alert(l);
+    var a = document.getElementById("final");
+    a.setAttribute("style","background:"+l.substring(0,l.length-1)+";")
   }
